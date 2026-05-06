fe38d9a8a2c9347655180bbc1010ab5cb7d2b085
aldrichdelapena44-stack <aldrichdelapena44@gmail.com>
2026-05-06 08:39:27 +0800

    Update and rename app/page.tsx to src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Age of Scent</h1>
+      <p>Website is deployed successfully.</p>
+    </main>
+  );
+}
